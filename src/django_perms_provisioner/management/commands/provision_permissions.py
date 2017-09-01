c6b3d2af210201e37448cf38caebdeeeb8dffee4
Nando Davis <nandodavis@gmail.com>
2017-09-01 13:41:25 +0200

    Yaml file generate

--- src/django_perms_provisioner/management/commands/provision_permissions.py
+++ src/django_perms_provisioner/management/commands/provision_permissions.py
@@ -4,13 +4,25 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-    """ command """
+    """ Generate yaml file from groups and permissions"""
 
     def add_arguments(self, parser):
         parser.add_argument('file_name', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        """ s """
+        """ Generate the yaml file """
         yaml_file = open(options['file_name'][0], 'w')
-        
+        yaml_file.write("---")
+        yaml_file.write('\n')
+        for group in Group.objects.all():
+            yaml_file.write(group.name + ":\n")
+            for permission in group.permissions.all():
+                yaml_file.write('  "')
+                yaml_file.write(permission.content_type.app_label)
+                yaml_file.write('.')
+                yaml_file.write(permission.codename)
+                yaml_file.write('": ')
+                yaml_file.write('"')
+                yaml_file.write(permission.content_type.model)
+                yaml_file.write('"\n')
         yaml_file.close()
