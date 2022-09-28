c9920c26b1ceb072550fc1fa19fd00c249e5b602
Mike Dingjan <m.dingjan@labdigital.nl>
2022-09-28 10:57:01 +0200

    Update python and package versions

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 
 tests_require = [
     "coverage",
-    "pytest==3.8.1",
-    "pytest-cov==2.6.0",
-    "pytest-django==3.4.3",
+    "pytest==7.1.3",
+    "pytest-cov==3.0.0",
+    "pytest-django==4.5.2",
     # Linting
-    "isort==4.2.5",
-    "flake8==3.5.0",
-    "flake8-blind-except==0.1.1",
-    "flake8-debugger==3.1.0",
+    "isort==5.10.1",
+    "flake8==5.0.4",
+    "flake8-blind-except==0.2.1",
+    "flake8-debugger==4.1.2",
 ]
 
 setup(
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/labd/django-perms-provisioner",
     author="Lab Digital",
     author_email="opensource@labdigital.nl",
-    install_requires=["Cerberus>=1.3.2,<2", "Django>=1.11", "PyYAML>=5.3.1,<7"],
+    install_requires=["Cerberus>=1.3.2,<2", "Django>=3.2", "PyYAML>=5.3.1,<7"],
     tests_require=tests_require,
     extras_require={"test": tests_require},
     entry_points={},
@@ -33,16 +33,16 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     zip_safe=False,
 )
