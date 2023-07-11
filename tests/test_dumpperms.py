cefcbe58dc2374b825fce7dfb9fc374761edbd9f
Manolis Stamatogiannakis <mstamat@gmail.com>
2023-07-11 18:20:01 +0200

    Added tests for dumpperms --sort/-S flag.

--- tests/test_dumpperms.py
+++ tests/test_dumpperms.py
@@ -11,20 +11,23 @@ STR_LOADER_MAPPING = {"yaml": yaml.safe_load, "json": json.loads}
 
 @pytest.mark.django_db
 class TestDumpperms:
-    @pytest.mark.parametrize("input_file, expected_file", [
-        ("valid.yaml", "valid.yaml"),
-        ("valid.json", "valid.json"),
+    @pytest.mark.parametrize("input_file, expected_file, extra_args", [
+        ("valid.yaml", "valid.yaml", None),
+        ("valid.json", "valid.json", None),
+        ("valid.json", "valid-sorted.json", ["-S"]),
+        ("valid.yaml", "valid-sorted.yaml", ["-S"]),
     ])
-    def test_valid(self, input_file, expected_file, capsys):
+    def test_valid(self, input_file, expected_file, extra_args, capsys):
         input_file = Path(__file__).parent / "files" / input_file
         expected_file = Path(__file__).parent / "files" / expected_file
+        extra_args = extra_args or []
 
         # first read the permissions to the DB
         call_command("loadperms", input_file)
         capsys.readouterr()  # Reset stdout.
 
-        # dump and capture the permissions to stdout and read them back
-        call_command("dumpperms", f"--format={expected_file.suffix[1:]}")
+        # dump and capture the permissions to stdout
+        call_command("dumpperms", f"--format={expected_file.suffix[1:]}", *extra_args)
         captured = capsys.readouterr()
 
         loader = FILE_LOADER_MAPPING[expected_file.suffix[1:]]
