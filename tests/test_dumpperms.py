93210128030ef9aeb8dd2865092884e0892ffa4f
Manolis Stamatogiannakis <mstamat@gmail.com>
2023-07-11 17:54:14 +0200

    Modified dumpperms test to separate input and expected output files.

--- tests/test_dumpperms.py
+++ tests/test_dumpperms.py
@@ -1,5 +1,5 @@
 import json
-import os
+from pathlib import Path
 
 import pytest
 import yaml
@@ -11,22 +11,25 @@ STR_LOADER_MAPPING = {"yaml": yaml.safe_load, "json": json.loads}
 
 @pytest.mark.django_db
 class TestDumpperms:
-    @pytest.mark.parametrize("filename", ["valid.yaml", "valid.json"])
-    def test_valid(self, filename, capsys):
-        name, ext = os.path.splitext(filename)
+    @pytest.mark.parametrize("input_file, expected_file", [
+        ("valid.yaml", "valid.yaml"),
+        ("valid.json", "valid.json"),
+    ])
+    def test_valid(self, input_file, expected_file, capsys):
+        input_file = Path(__file__).parent / "files" / input_file
+        expected_file = Path(__file__).parent / "files" / expected_file
 
         # first read the permissions to the DB
-        filename = os.path.join(os.path.dirname(__file__), "files", filename)
-        call_command("loadperms", filename)
+        call_command("loadperms", input_file)
         capsys.readouterr()  # Reset stdout.
 
-        # dump and capture the permissions to stdout
-        call_command("dumpperms", f"--format={ext[1:]}")
+        # dump and capture the permissions to stdout and read them back
+        call_command("dumpperms", f"--format={expected_file.suffix[1:]}")
         captured = capsys.readouterr()
 
-        loader = FILE_LOADER_MAPPING[ext[1:]]
-        with open(filename, "r") as stream:
-            contents = loader(stream)
+        loader = FILE_LOADER_MAPPING[expected_file.suffix[1:]]
+        with expected_file.open("r") as stream:
+            expected_contents = loader(stream)
 
-        str_loader = STR_LOADER_MAPPING[ext[1:]]
-        assert str_loader(captured.out) == contents, captured.out
+        str_loader = STR_LOADER_MAPPING[expected_file.suffix[1:]]
+        assert str_loader(captured.out) == expected_contents, captured.out
